e0667cffb97aea0b0e9a436c8120efb928b4df77
Victor Ribeiro <victorribeiro172@gmail.com>
2023-01-22 11:11:10 -0300

    funcionamento das duas buscas cegas

--- buscaEmLargura.py
+++ buscaEmLargura.py
@@ -1,5 +1,6 @@
 import queue
 import numpy as np
+from utils import estadosPossiveis
 
 def buscaEmLargura(estadoInicial, estadoFinal):
     nosGerados = 0
@@ -15,10 +16,10 @@ def buscaEmLargura(estadoInicial, estadoFinal):
         estadoAtual, profundidade = fronteiraEstados.get()
         estadosVisitados.add(tuple(estadoAtual))
         #Se a solução foi encontrada.
+        profundidadeMaxima = max(profundidadeMaxima, profundidade)
         if estadoAtual == estadoFinal:
             profundidadeSolucao = profundidade
             return estadoAtual, fronteiraEstados.qsize(), nosGerados, profundidadeMaxima, profundidadeSolucao, len(estadosVisitados)
-        profundidadeMaxima = max(profundidadeMaxima, profundidade)
         #Se ainda não foi, o nó é ampliado.
         for proximoEstado in estadosPossiveis(estadoAtual):
             print('Proximo estado:\n', np.array(proximoEstado).reshape(3,3), '\n///////////')
@@ -29,34 +30,6 @@ def buscaEmLargura(estadoInicial, estadoFinal):
             else:
                 print('O estado {} ja foi visitado'.format(tuple(proximoEstado)))
     return None
-
-def estadosPossiveis(estadoAtual):
-    espacoEmBranco = estadoAtual.index(0)
-    print('espacoEmBranco: ',espacoEmBranco)
-    x, y = espacoEmBranco // 3, espacoEmBranco % 3
-    print('X: ', x ,'Y: ', y)
-    movimentacoes = []
-    if x > 0:
-        movePraBaixo = estadoAtual[:]
-        movePraBaixo[espacoEmBranco], movePraBaixo[espacoEmBranco-3] = movePraBaixo[espacoEmBranco-3], movePraBaixo[espacoEmBranco]
-        print('movePraBaixo: \n',np.array(movePraBaixo).reshape(3,3))
-        movimentacoes.append(movePraBaixo)
-    if x < 2:
-        movePraCima = estadoAtual[:]
-        movePraCima[espacoEmBranco], movePraCima[espacoEmBranco+3] = movePraCima[espacoEmBranco+3], movePraCima[espacoEmBranco]
-        print('movePraCima: \n',np.array(movePraCima).reshape(3,3))
-        movimentacoes.append(movePraCima)
-    if y > 0:
-        movePraDireita = estadoAtual[:]
-        print('movePraDireita: \n',np.array(movePraDireita).reshape(3,3))
-        movimentacoes.append(movePraDireita)
-    if y < 2:
-        movePraEsquerda = estadoAtual[:]
-        movePraEsquerda[espacoEmBranco], movePraEsquerda[espacoEmBranco+1] = movePraEsquerda[espacoEmBranco+1], movePraEsquerda[espacoEmBranco]
-        print('movePraEsquerda: \n',np.array(movePraEsquerda).reshape(3,3))
-        movimentacoes.append(movePraEsquerda)
-    return movimentacoes
-
 
 def main():
     # exemplo de teste
---
+++ utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def estadosPossiveis(estadoAtual):
+    espacoEmBranco = estadoAtual.index(0)
+    print('espacoEmBranco: ',espacoEmBranco)
+    x, y = espacoEmBranco // 3, espacoEmBranco % 3
+    print('X: ', x ,'Y: ', y)
+    movimentacoes = []
+    if x > 0:
+        movePraBaixo = estadoAtual[:]
+        movePraBaixo[espacoEmBranco], movePraBaixo[espacoEmBranco-3] = movePraBaixo[espacoEmBranco-3], movePraBaixo[espacoEmBranco]
+        print('movePraBaixo: \n',np.array(movePraBaixo).reshape(3,3))
+        movimentacoes.append(movePraBaixo)
+    if x < 2:
+        movePraCima = estadoAtual[:]
+        movePraCima[espacoEmBranco], movePraCima[espacoEmBranco+3] = movePraCima[espacoEmBranco+3], movePraCima[espacoEmBranco]
+        print('movePraCima: \n',np.array(movePraCima).reshape(3,3))
+        movimentacoes.append(movePraCima)
+    if y > 0:
+        movePraDireita = estadoAtual[:]
+        print('movePraDireita: \n',np.array(movePraDireita).reshape(3,3))
+        movimentacoes.append(movePraDireita)
+    if y < 2:
+        movePraEsquerda = estadoAtual[:]
+        movePraEsquerda[espacoEmBranco], movePraEsquerda[espacoEmBranco+1] = movePraEsquerda[espacoEmBranco+1], movePraEsquerda[espacoEmBranco]
+        print('movePraEsquerda: \n',np.array(movePraEsquerda).reshape(3,3))
+        movimentacoes.append(movePraEsquerda)
+    return movimentacoes
