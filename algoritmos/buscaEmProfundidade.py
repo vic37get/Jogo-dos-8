80764f6f6825aac51dd40a082f95ef402de75715
Victor <victorribeiro172@gmail.com>
2023-01-23 19:20:47 -0300

    melhoria profundidade

--- algoritmos/buscaEmProfundidade.py
+++ algoritmos/buscaEmProfundidade.py
@@ -2,9 +2,11 @@ import numpy as np
 from utils import estadosPossiveis
 
 
-def buscaEmLargura(estadoInicial, estadoFinal):
+def buscaEmProfundidadeMain(estadoInicial, estadoFinal):
+    iteracoes = []
     nosGerados = 0
     profundidadeMaxima = 0
+    jogada = 0
     profundidadeSolucao = None
     fronteiraEstados = []
     fronteiraEstados.append((estadoInicial, 0))
@@ -12,67 +14,36 @@ def buscaEmLargura(estadoInicial, estadoFinal):
     estadosVisitados.add(tuple(estadoInicial))
 
     while len(fronteiraEstados) != 0:
+        jogada+=1
         print("ESPAÇO DE ESTADOS: \n", fronteiraEstados)
         estadoAtual, profundidade = fronteiraEstados.pop()
         print("ESTADO ATUAL: \n", np.array(estadoAtual).reshape(3, 3))
         estadosVisitados.add(tuple(estadoAtual))
+        possiveisJogadas = estadosPossiveis(estadoAtual)
+
         # Se a solução foi encontrada.
         profundidadeMaxima = max(profundidadeMaxima, profundidade)
         if estadoAtual == estadoFinal:
             profundidadeSolucao = profundidade
+            iteracoes.append([estadoAtual, '', (len(estadosVisitados) + nosGerados), nosGerados, len(fronteiraEstados), profundidadeSolucao, profundidadeMaxima, jogada])
             return (
                 estadoAtual,
                 len(fronteiraEstados),
                 nosGerados,
                 profundidadeMaxima,
                 profundidadeSolucao,
-                len(estadosVisitados),
+                len(estadosVisitados), iteracoes
             )
         # Se ainda não foi, o nó é ampliado.
-        for proximoEstado in estadosPossiveis(estadoAtual):
-            print(
-                "Proximo estado:\n",
-                np.array(proximoEstado).reshape(3, 3),
-                "\n///////////",
-            )
+        estadosNaoRepetidos = []
+        for proximoEstado in possiveisJogadas:
+            print("Proximo estado:\n",np.array(proximoEstado).reshape(3, 3),"\n///////////",)
             if tuple(proximoEstado) not in estadosVisitados:
                 # Adicionando na fronteira de espaço de estados.
                 fronteiraEstados.append((proximoEstado, profundidade + 1))
+                estadosNaoRepetidos.append(proximoEstado)
                 nosGerados += 1
             else:
                 print("O estado {} ja foi visitado".format(tuple(proximoEstado)))
+        iteracoes.append([estadoAtual, estadosNaoRepetidos, (len(estadosVisitados) + nosGerados), nosGerados, len(fronteiraEstados), profundidadeSolucao, profundidadeMaxima, jogada])
     return None
-
-
-def main():
-    # exemplo de teste
-    estadoInicial = [1, 2, 3, 4, 5, 6, 0, 7, 8]
-    estadoFinal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-    (
-        solucao,
-        fronteira,
-        nosGerados,
-        profundidadeMaxima,
-        profundidadeSolucao,
-        estadosVisitados,
-    ) = buscaEmLargura(estadoInicial, estadoFinal)
-    if solucao:
-        print(
-            "Solução: ",
-            solucao,
-            "\nFronteira de espaço de estados: ",
-            fronteira,
-            "\nNós Gerados: ",
-            nosGerados,
-            "\nProfundidade Maxima: ",
-            profundidadeMaxima,
-            "\nProfundidade da Solução: ",
-            profundidadeSolucao,
-            "\nTempo gasto: ",
-            estadosVisitados + nosGerados,
-        )
-    else:
-        print("Não foi possível encontrar solução.")
-
-
-main()
--- jogoApp/views.py
+++ jogoApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from algoritmos.buscaEmLargura import buscaEmLarguraMain
+from algoritmos.buscaEmProfundidade import buscaEmProfundidadeMain
 
 # Create your views here.
 def home(request):
@@ -14,16 +15,19 @@ def buscaEmLargura(request):
     estadoFinal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
     solucao, fronteira, nosGerados, profundidadeMaxima, profundidadeSolucao, estadosVisitados, iteracoes = buscaEmLarguraMain(estadoInicial, estadoFinal)
     context = {
-        'tipoBusca': 'Largura',
+        'tipoBusca': 'Busca em Largura',
         'tabuleiro': iteracoes,
     }
-    print(iteracoes)
     template = loader.get_template('jogoApp/jogoDos8.html')
     return HttpResponse(template.render(context, request))
 
 def buscaEmProfundidade(request):
+    estadoInicial = [1, 2, 3, 4, 5, 6, 0, 7, 8]
+    estadoFinal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    solucao, fronteira, nosGerados, profundidadeMaxima, profundidadeSolucao, estadosVisitados, iteracoes = buscaEmProfundidadeMain(estadoInicial, estadoFinal)
     context = {
-        'tipoBusca': 'Profundidade',
+        'tipoBusca': 'Busca em Profundidade',
+        'tabuleiro': iteracoes,
     }
     template = loader.get_template('jogoApp/jogoDos8.html')
     return HttpResponse(template.render(context, request))
