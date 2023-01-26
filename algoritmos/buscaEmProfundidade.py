b6725f4fdb67381a453be24e00c4388bd1c9a1cf
Victor Ribeiro da Silva <51518489+vic37get@users.noreply.github.com>
2023-01-26 13:32:20 -0300

    Update buscaEmProfundidade.py

--- algoritmos/buscaEmProfundidade.py
+++ algoritmos/buscaEmProfundidade.py
@@ -1,53 +1,47 @@
 import numpy as np
-from funcoesAuxiliares import estadosPossiveisBuscaCega
-
+from funcoesAuxiliares import *
 
 def buscaEmProfundidadeMain(estadoInicial, estadoFinal):
     iteracoes = []
     nosGerados = 0
-
     custoCaminho = 0
     tam_fronteira = []
-
     profundidadeMaxima = 0
-    profundidadeSolucao = 0
     fronteiraEstados = []
-    fronteiraEstados.append((estadoInicial, 0))
+    fronteiraEstados.append(estado(estadoInicial, None, 0, None))
     estadosVisitados = []
 
     while len(fronteiraEstados) != 0:
         tam_fronteira.append(len(fronteiraEstados))
-        estadoAtual, profundidade = fronteiraEstados.pop()
-        if estadoAtual not in estadosVisitados:
-            estadosVisitados.append(estadoAtual)
+        estadoAtual = fronteiraEstados.pop()
+        if estadoAtual.estado not in estadosVisitados:
+            estadosVisitados.append(estadoAtual.estado)
         custoCaminho+=1
-        print('\nEstado atual: \n', np.array(estadoAtual).reshape(3,3))
-        print('Profundidade: ', profundidade)
-        possiveisJogadas = estadosPossiveisBuscaCega(estadoAtual)
+        print('\nEstado atual: \n', np.array(estadoAtual.estado).reshape(3,3))
+        print('Profundidade: ', estadoAtual.g)
+        possiveisJogadas = estadosPossiveis(estadoAtual.estado)
         # Se a solução foi encontrada.
-        profundidadeMaxima = max(profundidadeMaxima, profundidade)
-        if estadoAtual == estadoFinal:
-            profundidadeSolucao = profundidade
-            iteracoes.append([estadoAtual, '', custoCaminho, len(fronteiraEstados), nosGerados, profundidadeSolucao, profundidadeMaxima])
+        profundidadeMaxima = max(profundidadeMaxima, estadoAtual.g)
+        if estadoAtual.estado == estadoFinal:
+            iteracoes.append([estadoAtual.estado, '', custoCaminho, len(fronteiraEstados), nosGerados, estadoAtual.g, profundidadeMaxima])
             #Estado Atual, Custo do caminho, Custo de espaço, Custo de tempo.
-            return estadoAtual, custoCaminho, max(tam_fronteira), custoCaminho, profundidadeSolucao, profundidadeMaxima, iteracoes
+            return estadoAtual.estado, custoCaminho, max(tam_fronteira), custoCaminho, estadoAtual.g, profundidadeMaxima, iteracoes
         # Se ainda não foi, o nó é ampliado.
         estadosIteracoes = []
         for proximoEstado in possiveisJogadas:
             if proximoEstado not in estadosVisitados:
                 # Adicionando na fronteira de espaço de estados.
-                fronteiraEstados.append((proximoEstado, profundidade + 1))
+                fronteiraEstados.append(estado(proximoEstado, estadoAtual, estadoAtual.g+1, None))
                 estadosVisitados.append(proximoEstado)
                 print('Estado gerado: \n', np.array(proximoEstado).reshape(3,3))
-                print('Profundidade: ', profundidade+1)
+                print('Profundidade: ', estadoAtual.g+1)
                 estadosIteracoes.append(np.array(proximoEstado).reshape(1,-1))
                 nosGerados+=1
             else:
                 print("O estado {} ja foi visitado".format(tuple(proximoEstado)))
-        iteracoes.append([estadoAtual, estadosIteracoes, custoCaminho, len(fronteiraEstados), nosGerados, profundidadeSolucao, profundidadeMaxima])
+        iteracoes.append([estadoAtual.estado, estadosIteracoes, custoCaminho, len(fronteiraEstados), nosGerados, estadoAtual.g, profundidadeMaxima])
     return None
 
-
-'''estadoInicial = [[1,2,3],[4,5,6],[0,7,8]]
+'''estadoInicial = [[4,1,3],[7,2,5],[0,8,6]]
 estadoFinal = [[1,2,3],[4,5,6],[7,8,0]]
 print(buscaEmProfundidadeMain(estadoInicial, estadoFinal))'''
